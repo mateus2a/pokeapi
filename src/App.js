a599774a5602ecc001193edb0edb5181b07689e4
Mateus Alencar <mtauws@gmail.com>
2021-09-30 22:37:48 -0300

    UPDATE dependences

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 class App extends Component {
-  render() { 
-    return <h1>App</h1>;
+  render() {
+    return <h1>App</h1>
   }
 }
- 
-export default App;
+
+export default App
